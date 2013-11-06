3d4c8efb6d81fc1b7f61f8d1df1632e3f8057364
Stanimir Nikolov <stanimirnikolov@stanimirs-mini.net1.bg>
2013-11-06 11:12:49 +0200

    Some improvements in base64 encoding.

--- DatingTips/CommunicationManager.m
+++ DatingTips/CommunicationManager.m
@@ -11,7 +11,6 @@
 #import "Constants.h"
 #import "NSData+AES.h"
 #import <StoreKit/StoreKit.h>
-#import "NSData+Base64.h"
 
 static CommunicationManager* sharedManager = nil;
 NSString* urlSecret;
@@ -215,33 +214,6 @@ NSString* urlSecret;
     }
 }
 
-
-- (NSString*)base64forData:(NSData*)theData {
-    const uint8_t* input = (const uint8_t*)[theData bytes];
-    NSInteger length = [theData length];
-    static char table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-    NSMutableData* data = [NSMutableData dataWithLength:((length + 2) / 3) * 4];
-    uint8_t* output = (uint8_t*)data.mutableBytes;
-    NSInteger i;
-    for (i=0; i < length; i += 3) {
-        NSInteger value = 0;
-        NSInteger j;
-        for (j = i; j < (i + 3); j++) {
-            value <<= 8;
-            
-            if (j < length) {
-                value |= (0xFF & input[j]);
-            }
-        }
-        NSInteger theIndex = (i / 3) * 4;
-        output[theIndex + 0] =                    table[(value >> 18) & 0x3F];
-        output[theIndex + 1] =                    table[(value >> 12) & 0x3F];
-        output[theIndex + 2] = (i + 1) < length ? table[(value >> 6)  & 0x3F] : '=';
-        output[theIndex + 3] = (i + 2) < length ? table[(value >> 0)  & 0x3F] : '=';
-    }
-    return [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
-}
-
 - (NSDictionary *) getDictionaryFromJsonString:(NSString *)jsonstring {
     NSError *jsonError;
     NSDictionary *dictionary = (NSDictionary *) [NSJSONSerialization JSONObjectWithData:[jsonstring dataUsingEncoding:NSUTF8StringEncoding] options:0 error:&jsonError];
@@ -262,21 +234,20 @@ NSString* urlSecret;
     [request setValue:self.sessionId forHTTPHeaderField:@"X-APP-SESSION"];
 
     //set the receipt
-    //NSString* receiptDataString = [self base64forData:receiptData];
-    
-    NSString *receiptBase64String = [self encodeBase64:(uint8_t *)receiptData.bytes
-                                             length:receiptData.length];
-
-//    NSDictionary* dict = @{@"receipt-data":receiptBase64String};
-//    NSData* jsonStringData =[NSJSONSerialization dataWithJSONObject:dict options:0 error:nil];
-    
-    
+    NSString *receiptBase64String = nil;
+    if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_6_1) {
+        // Load resources for iOS 6.1 or earlier
+        receiptBase64String = [receiptData base64Encoding];
+    }
+    else{
+        // Load resources for iOS 7 or later
+        receiptBase64String = [receiptData base64EncodedStringWithOptions:0];
+    }
+//    receiptBase64String = [self encodeBase64:(uint8_t *)receiptData.bytes length:receiptData.length];
     NSData* data = [receiptBase64String dataUsingEncoding:NSUTF8StringEncoding];
-    
-//    NSString* requestBodyString = [NSString stringWithFormat:@"payment={\"receipt-data\" : \"%@\"}",jsonObjectString];
-//    NSData* bodyData = [requestBodyString dataUsingEncoding:NSUTF8StringEncoding];
     [request setHTTPBody:data];
     
+    //send the request
     NSData* tipsData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:error];
     if(!*error){
         if(!tipsData){
@@ -305,34 +276,6 @@ NSString* urlSecret;
     }
     return nil;
 }
-
-#pragma mark
-#pragma mark Base 64 encoding
-
-- (NSString *)encodeBase64:(const uint8_t *)input length:(NSInteger)length
-{
-    NSData * data = [NSData dataWithBytes:input length:length];
-    return [data base64EncodedString];
-}
-
-
-- (NSString *)decodeBase64:(NSString *)input length:(NSInteger *)length
-{
-    NSData * data = [NSData dataFromBase64String:input];
-    return [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-}
-
-char* base64_encode(const void* buf, size_t size) {
-    size_t outputLength;
-    return NewBase64Encode(buf, size, NO, &outputLength);
-}
-
-void * base64_decode(const char* s, size_t * data_len)
-{
-    return NewBase64Decode(s, strlen(s), data_len);
-}
-
-
 
 #pragma mark - Public Methods
 
